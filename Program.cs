64a14697382cdbabddf35bb0b404dd5078fe37c0
PavelM <manankov.pavel@gmail.com>
2024-02-25 16:55:20 +0300

    create program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] CreateStringArray()
+{
+    string[] array={"Hello", "2", "world", ":-)"};
+    //string[] array={"1234", "1567", "-2", "computer science"};
+    //string[] array={"Russia", "Denmark", "Kazan"};
+    return array;
+}
+
+void PrintStringArray(string[] array)
+{
+    for (int i=0;i<array.Length;i++){
+        Console.Write($"{array[i]} ");
+        Console.Write($" ");
+    }
+}
+
+string[] ParseStringArray(string[] array,int amountChar)
+{
+    int sizeNewArray=0;
+    for(int i=0;i<array.Length;i++){
+        if (array[i].Length<=amountChar)
+        {
+            sizeNewArray++;
+        }
+    }
+    string[] newArray=new string[sizeNewArray];
+    int index=0;
+    for(int i=0;i<array.Length;i++){
+        if (array[i].Length<=amountChar)
+        {
+            newArray[index]=array[i];
+            index++;
+        }
+    }
+    return newArray;
+}
+
+int amountChar=3;
+string[] array=CreateStringArray();
+array=ParseStringArray(array,amountChar);
+PrintStringArray(array);
